2db107d918b5e9f8268a8bcee64200e4356f2746
kasiriveni <kasiriveni@gmail.com>
2023-09-24 13:20:12 +0530

    commit1

---
+++ src/main/java/com/javatpoint/springbootjpaexample/SpringBootJpaExampleApplication.java
@@ -0,0 +1,14 @@
+package com.javatpoint.springbootjpaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringBootJpaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaExampleApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+create table if not exists userdata (rno int,uname varchar(20));
+insert into userdata values(101,'Tom');
+insert into userdata values(102,'Andrew');
+insert into userdata values(103,'Tony');
+insert into userdata values(104,'Bob');
+insert into userdata values(105,'Sam');
